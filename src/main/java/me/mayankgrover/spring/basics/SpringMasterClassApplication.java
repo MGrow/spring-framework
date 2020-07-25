9a231296086627ea8a192cbd09ef1602bc2a9030
Mayank Grover <mgrow4@gmail.com>
2020-07-25 13:10:02 -0400

    Init commit - Spring boot setup

---
+++ src/main/java/me/mayankgrover/spring/basics/SpringMasterClassApplication.java
@@ -0,0 +1,13 @@
+package me.mayankgrover.spring.basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMasterClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMasterClassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/mayankgrover/spring/basics/SpringMasterClassApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mayankgrover.spring.basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMasterClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
