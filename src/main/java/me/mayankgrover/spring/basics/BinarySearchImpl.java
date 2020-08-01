c09d5fbe64e0b307cfe950453d1064f6a190ba0b
MGrow <mgrow4@gmail.com>
2020-08-01 13:12:23 -0400

    In the absence of @Primary, beans are matched by class names. Here, changing the var name to quickSortAlgorithm uses the QuickSortAlgorithm bean.

--- src/main/java/me/mayankgrover/spring/basics/BinarySearchImpl.java
+++ src/main/java/me/mayankgrover/spring/basics/BinarySearchImpl.java
@@ -6,20 +6,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class BinarySearchImpl {
 
-    private SortAlgorithm sortAlgo;
-
     @Autowired
-    public void setSortAlgo(SortAlgorithm sortAlgo) {
-        System.out.println("Hey");
-        this.sortAlgo = sortAlgo;
-    }
+    private SortAlgorithm quickSortAlgorithm;
 
     public int binarySearch(int[] nums, int searchVal) {
 
         // Sorting the array
-        int[] sortedNums = sortAlgo.sort(nums);
+        int[] sortedNums = quickSortAlgorithm.sort(nums);
         // Print sort obj being used
-        System.out.println(sortAlgo);
+        System.out.println(quickSortAlgorithm);
 
         // Search: Assume we have searched
 
