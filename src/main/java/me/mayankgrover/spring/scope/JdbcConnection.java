c5fba783fc0f3a22d4bee349aea5631c640f3b98
MGrow <mgrow4@gmail.com>
2020-08-01 17:28:55 -0400

    ScopedProxyMode.TARGET_CLASS forces the consuming bean to generate a new instance of this class even if the consuming class is a singleton

--- src/main/java/me/mayankgrover/spring/scope/JdbcConnection.java
+++ src/main/java/me/mayankgrover/spring/scope/JdbcConnection.java
@@ -2,10 +2,12 @@ package me.mayankgrover.spring.scope;
 
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE,
+        proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class JdbcConnection {
     public JdbcConnection() {
         System.out.println("JDBC Connection");
