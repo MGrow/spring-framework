0a7d4d32efda5d12a2d6f65dddd3e76a63dd4639
MGrow <mgrow4@gmail.com>
2020-08-02 16:29:18 -0400

    @PostConstruct mentioned on a method signifies to Spring to call the method right after Bean construction. @PreDestroy methods are used as callbacks, called by Spring right before destroying the bean.

--- src/main/java/me/mayankgrover/spring/in28minutes/basics/BinarySearchImpl.java
+++ src/main/java/me/mayankgrover/spring/in28minutes/basics/BinarySearchImpl.java
@@ -1,14 +1,21 @@
 package me.mayankgrover.spring.in28minutes.basics;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BinarySearchImpl {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(BinarySearchImpl.class);
 
     @Autowired
     @Qualifier("quickSort")
@@ -25,5 +32,17 @@ public class BinarySearchImpl {
 
         // Return value (exists/does not/index .. whatever)
         return 3;
+    }
+
+    @PostConstruct
+    public int something() {
+        LOGGER.info("Post Construct");
+        return 0;
+    }
+
+    @PreDestroy
+    public int somethingElse() {
+        LOGGER.info("Pre Destroy");
+        return 0;
     }
 }
