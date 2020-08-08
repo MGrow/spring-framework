a77005e19491631b159a4ed34ac761e49a587918
MGrow <mgrow4@gmail.com>
2020-08-08 16:57:18 -0400

    @Component Annotations: @Repository, @Service, @Controller -- for different specific purposes in the application.

--- src/main/java/me/mayankgrover/spring/in28minutes/basics/BubbleSortAlgorithm.java
+++ src/main/java/me/mayankgrover/spring/in28minutes/basics/BubbleSortAlgorithm.java
@@ -2,8 +2,9 @@ package me.mayankgrover.spring.in28minutes.basics;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 @Qualifier("bubbleSort")
 public class BubbleSortAlgorithm implements SortAlgorithm {
     public int[] sort(int[] numbers) {
--- src/main/java/me/mayankgrover/spring/in28minutes/basics/QuickSortAlgorithm.java
+++ src/main/java/me/mayankgrover/spring/in28minutes/basics/QuickSortAlgorithm.java
@@ -2,8 +2,9 @@ package me.mayankgrover.spring.in28minutes.basics;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 @Qualifier("quickSort")
 public class QuickSortAlgorithm implements SortAlgorithm {
 
